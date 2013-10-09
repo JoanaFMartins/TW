72f44e79cc33425434a9b250f372709ed979b558
MiniLuZ <moreirinha1@hotmail.com>
2013-10-09 10:28:05 +0100

    ...

--- EducationalRocketKitten/public_html/main.js
+++ EducationalRocketKitten/public_html/main.js
@@ -13,3 +13,11 @@ function hide(){
 function errorMessage(){
 	alert("Esta opção ainda não está disponível.");
 }
+
+function validateGameMode(){
+	var type = document.getElementById("gType");
+	var mode = document.getElementById("gMode");
+	if(!type.selected || !mode.selected){
+		alert("Apenas o modo individual e o tipo de jogo aritmética estão disponíveis.");
+	}
+}
